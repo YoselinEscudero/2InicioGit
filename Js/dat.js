2007956d951b9d9d1397745d63fe59d68948282e
YoselinEscudero <y.escuderohernandez@ugto.mx>
2023-02-08 15:27:35 -0600

    Agregamos mas css

---
+++ Js/dat.js
@@ -0,0 +1,32 @@
+const songs=[
+    {
+        name: 'Best part',
+        path: '../music/Song 1.mp3',
+        artist: "Marco",
+        cover: '../images/cover 1.png'
+    },
+    {
+        name: 'Lover',
+        path: '../music/Song 2.mp3',
+        artist: "Marco",
+        cover: '../images/cover 2.png'
+    },
+    {
+        name: 'GoodBye',
+        path: '../music/Song 3.mp3',
+        artist: "Marco",
+        cover: '../images/cover 3.png'
+    },
+    {
+        name: 'Neverita',
+        path: '../music/Song 4.mp3',
+        artist: "Marco",
+        cover: '../images/cover 4.png'
+    },
+    {
+        name: 'La corriente',
+        path: '../music/Song 5.mp3',
+        artist: "Marco",
+        cover: '../images/cover 5.png'
+    }
+]
