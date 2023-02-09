f4e2d5fdd5baf53d518945334b75c2a5fb6cdbfc
YoselinEscudero <y.escuderohernandez@ugto.mx>
2023-02-08 22:43:47 -0600

    agregamos unas cositas

--- Js/app.js
+++ Js/app.js
@@ -1,57 +1,57 @@
 let currentMusic=0;
 
-const music=document.querySelector('#audio')
-const seekBar=document.querySelector('.seek-bar')
-const songName= document.querySelector('.music-name')
-const artistName= document.querySelector('.artist-name')
-const disk=document.querySelector('.disk')
-const currentTime= document.querySelector('.current-time')
-const musicDuration= document.querySelector('.song-duration')
-const playBtn=document.querySelector('.play-btn')
-const forwardBtn= document.querySelector('.forward-btn')
-const backwardBtn=document.querySelector('.backward-btn')
+const music = document.querySelector('#audio')
+const seekBar = document.querySelector('.seek-bar')
+const songName = document.querySelector('.music-name')
+const artistName = document.querySelector('.artist-name')
+const disk = document.querySelector('.disk')
+const currentTime = document.querySelector('.current-time')
+const musicDuration = document.querySelector('.song-duration')
+const playBtn = document.querySelector('.play-btn')
+const forwardBtn = document.querySelector('.forward-btn')
+const backwardBtn = document.querySelector('.backward-btn')
 
-playBtn.addEventListener('click',()=>{
+playBtn.addEventListener('click', () => {
     if(playBtn.className.includes('pause')){
         music.play()
-    } else{
+    } else {
     music.pause()
     }
     playBtn.classList.toggle('pause')
     disk.classList.toggle('play')
 })
 
-const setMusic=(i)=>{
-    seekBar.value=0;
-    let song=songs[i]
-    currentMusic=i
-    music.src=song.path
-    songName.innerHTML=song.name
-    artistName.innerHTML= song.artist
-    disk.style.backgroundImage=`url('${song.cover}')`
-    currentTime.innerHTML='00:00'
+const setMusic = (i) => {
+    seekBar.value = 0;
+    let song = songs[i]
+    currentMusic = i
+    music.src = song.path
+    songName.innerHTML = song.name
+    artistName.innerHTML = song.artist
+    disk.style.backgroundImage =`url('${song.cover}')`
+    currentTime.innerHTML ='00:00'
     //ponemos un delay 
     setTimeout(()=>{
-        seekBar.max=music.duration
+        seekBar.max = music.duration
         console.log('duracion',music.duration)
-        musicDuration.innerHTML=formatTime(music.Duration)
+        musicDuration.innerHTML = formatTime(music.Duration)
 
     },700)
 
 
 }
-setMusic(0)
+setMusic(3)
 
-const formatTime=(time)=>{
-    let min=Math.floor(time/60)
-    if(min<10){
-        min=`0${min}`
+const formatTime = (time) => {
+    let min = Math.floor(time / 60)
+    if(min < 10){
+        min =`0${ min }`
     }
-    let sec=Math.floor(time % 60)
-    if(sec<10){
-        sec=`0${sec}`
+    let sec = Math.floor(time % 60)
+    if(sec < 10){
+        sec =`0${ sec }`
     }
-    return `${min}: ${sec}`
+    return `${ min }: ${ sec }`
 }
 
 //Trabajar con el seek bar
